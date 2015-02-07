340d25e4fc5f2bb61a630bed376252f4adbbdb88
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2015-02-07 01:11:40 +0100

    removed obsolete routine that dealt with dedicated date struct (now using generic matlab datenum)

--- struct3mfe.m
+++
@@ -1,4 +0,0 @@
-function mfe=struct3mfe(datestruct,jdsatepoch)
-
-  jd = jday( datestruct.year,datestruct.mon,datestruct.day,datestruct.hr,datestruct.min,datestruct.sec );
-  mfe = (jd -jdsatepoch) * 1440.0;
